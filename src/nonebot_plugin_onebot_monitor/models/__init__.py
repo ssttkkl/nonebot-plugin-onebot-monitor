a663c1b0863f7fb796914d7185ba0a84505bb882
ssttkkl <huang.wen.long@hotmail.com>
2022-10-30 10:53:55 +0800

    修正nonebot_plugin_sqlalchemy加载

--- src/nonebot_plugin_onebot_monitor/models/__init__.py
+++ src/nonebot_plugin_onebot_monitor/models/__init__.py
@@ -1,7 +1,10 @@
-from nonebot import get_driver
-from nonebot_plugin_sqlalchemy import DataSource
+from nonebot import get_driver, require
 
 from ..config import conf
+
+require("nonebot_plugin_sqlalchemy")
+
+from nonebot_plugin_sqlalchemy import DataSource
 
 data_source = DataSource(get_driver(), conf.onebot_monitor_db_conn_url)
 
